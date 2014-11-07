c5bfaaaf919a60c4f542504390f71ff858e481c2
Roy Liu <carsomyr@gmail.com>
2014-11-06 20:39:11 -0500

    Add the `scalient-bootstrap` cookbook

---
+++ vendor/cookbooks/scalient-bootstrap/metadata.rb
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2014 Roy Liu
+# All rights reserved.
+
+name "scalient-bootstrap"
+maintainer "Roy Liu"
+maintainer_email "carsomyr@gmail.com"
+description "The organization-wide Scalient cookbook"
+long_description "The organization-wide Scalient cookbook."
+version "0.9.0"
+
+supports "mac_os_x"
+supports "mac_os_x_server"
+
+depends "osx-bootstrap"
