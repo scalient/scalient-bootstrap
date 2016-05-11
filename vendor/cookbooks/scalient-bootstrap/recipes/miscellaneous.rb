c67ff24488f078e2b901c588b3a6bf4a1f05c82b
Roy Liu <carsomyr@gmail.com>
2016-05-11 01:46:59 -0400

    Disable automatic brightness adjustment for the screen and keyboard; don't warn about applications freshly downloaded from the internet

--- vendor/cookbooks/scalient-bootstrap/recipes/miscellaneous.rb
+++ vendor/cookbooks/scalient-bootstrap/recipes/miscellaneous.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2014 Roy Liu
+# Copyright 2014-2016 Roy Liu
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
@@ -14,10 +14,40 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
+include_recipe "osx-bootstrap::preferences"
+
+prefs = node["scalient-bootstrap"]["miscellaneous"]
+
 service "com.apple.locate" do
+  # Enable the `locate.updatedb` service for building the `locate` database.
   action :enable
 end
 
 service "com.apple.rcd" do
+  # Don't start iTunes when the user presses the "Play" media key.
   action :disable
+end
+
+plist_file "com.apple.iokit.AmbientLightSensor" do
+  # Automatically adjust display and keyboard brightness.
+  adjust_brightness = prefs["ambient_light_sensor"]["adjust_brightness"]
+
+  set "Automatic Display Enabled", adjust_brightness
+  set "Automatic Keyboard Enabled", adjust_brightness
+
+  owner "root"
+  format :binary
+  action :update
+end
+
+plist_file "com.apple.LaunchServices" do
+  # Don't warn about applications freshly downloaded from the internet.
+  content(LSQuarantine: false)
+
+  format :binary
+
+  # We need to restart `Finder` for the changes to take effect.
+  notifies :run, "execute[`killall -- Finder`]", :immediately
+
+  action :create
 end
