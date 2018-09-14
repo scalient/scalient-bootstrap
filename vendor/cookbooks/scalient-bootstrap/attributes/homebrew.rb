845d38da8a602ddc50bfdeb813a48b22e2ea35a2
Roy Liu <carsomyr@gmail.com>
2018-09-14 16:37:38 -0400

    Install Google Chrome Beta instead of Google Chrome

--- vendor/cookbooks/scalient-bootstrap/attributes/homebrew.rb
+++ vendor/cookbooks/scalient-bootstrap/attributes/homebrew.rb
@@ -23,7 +23,7 @@ default["scalient-bootstrap"]["homebrew"]["formulas"] = [
 ]
 
 default["scalient-bootstrap"]["homebrew"]["casks"] = [
-    "google-chrome",
+    "google-chrome-beta",
     "google-drive-file-stream",
     "marked"
 ]
