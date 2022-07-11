4b0cd76b74cef01e6a23ee027e1ca5a365a770f3
Clark Slater <clark@scalient.io>
2022-07-10 22:03:12 -0400

    Update defaults

--- vendor/cookbooks/personal/attributes/default.rb
+++ vendor/cookbooks/personal/attributes/default.rb
@@ -3,4 +3,5 @@
 # Copyright 2014-2022 Clark Slater
 # All rights reserved.
 
-default["os-bootstrap"]["preferences"]["clock"]["format"] = "EEE d MMM h:mm:ss a"
+default["os-bootstrap"]["preferences"]["clock"]["format"] = "EEE d MMM h:mm a"
+default["os-bootstrap"]["preferences"]["dock"]["orientation"] = "bottom"
