0908b194ca84c42bb8ceb797bf2e1e54c0188d8c
Roy Liu <carsomyr@gmail.com>
2024-01-02 12:26:07 -0500

    Add the `gcloud` recipe

--- vendor/cookbooks/scalient-bootstrap/recipes/gcloud.rb
+++ vendor/cookbooks/scalient-bootstrap/recipes/gcloud.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
-#
-# Copyright 2014-2016 Roy Liu
+
+# Copyright 2024 Roy Liu
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
@@ -14,15 +14,24 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-include_recipe "os-bootstrap"
-include_recipe "os-bootstrap::editor"
-include_recipe "os-bootstrap::gnupg2"
-include_recipe "os-bootstrap::java"
-include_recipe "os-bootstrap::ruby"
-include_recipe "scalient-bootstrap::chef"
-include_recipe "scalient-bootstrap::gcloud"
-include_recipe "scalient-bootstrap::gnu_utils"
-include_recipe "scalient-bootstrap::homebrew"
-include_recipe "scalient-bootstrap::miscellaneous"
-include_recipe "scalient-bootstrap::ruby"
-include_recipe "scalient-bootstrap::ssh"
+require "shellwords"
+
+class << self
+  include Os::Bootstrap
+  include Os::Bootstrap::Homebrew
+end
+
+recipe = self
+
+homebrew_cask "google-cloud-sdk" do
+  action :update
+end
+
+template "#{owner_dir}/.profile.d/0011_gcloud.sh".to_s do
+  source "bash-0011_gcloud.sh.erb"
+  owner recipe.owner
+  group recipe.owner_group
+  mode 0o644
+  helper(:prefix) { recipe.homebrew_prefix }
+  action :create
+end
